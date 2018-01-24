00b2a7f9adb70fa5ff0891167c97a3945a45312a
Lerato Sambula <sambula.lerato4@gmail.com>
2018-01-24 09:54:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/admin/apptoast/Landing.java
@@ -0,0 +1,14 @@
+package com.example.admin.apptoast;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Landing extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+    }
+}
---
+++ app/src/main/java/com/example/admin/apptoast/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.admin.apptoast;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_login, btn_signup;
+    private TextView txt_forgotPW;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_login = findViewById(R.id.btn_login);
+        btn_signup = findViewById(R.id.btn_signup);
+        txt_forgotPW = findViewById(R.id.txt_forgotPW);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,Landing.class);
+                startActivity(i);
+            }
+        });
+
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,Signup.class);
+                startActivity(i);
+            }
+        });
+
+        txt_forgotPW.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,Forgot_Password.class);
+                startActivity(i);
+            }
+        });
+    }
+}
