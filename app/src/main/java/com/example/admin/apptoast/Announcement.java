234bd0cdc63afbdbe8c40f55f38985ce6f04fea2
Admin <rikhotsothoko@gmail.com>
2018-01-24 13:09:04 +0200

    Grid to lists

--- app/src/main/java/com/example/admin/apptoast/Announcement.java
+++ app/src/main/java/com/example/admin/apptoast/Announcement.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 public class Announcement extends AppCompatActivity {
 
 
-
+//
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
