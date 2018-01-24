aae8f2083c3042520f2014425ffd9023279ce911
Lerato Sambula <sambula.lerato4@gmail.com>
2018-01-24 10:29:40 +0200

    profile activity

--- app/src/main/java/com/example/admin/apptoast/Landing.java
+++ app/src/main/java/com/example/admin/apptoast/Landing.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class Landing extends AppCompatActivity{
 
     LinearLayout citytocity, putco, reavaya, metrobus;
+    ImageView profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,7 @@ public class Landing extends AppCompatActivity{
         putco = findViewById(R.id.layout_putco);
         reavaya = findViewById(R.id.layout_reavaya);
         metrobus = findViewById(R.id.layout_metrobus);
+        profile = findViewById(R.id.profile);
 
         citytocity.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,5 +55,12 @@ public class Landing extends AppCompatActivity{
             }
         });
 
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Landing.this,Profile.class);
+                startActivity(i);
+            }
+        });
     }
 }
