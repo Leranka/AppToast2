7fa2a474696beaa92c5c3f7f191e383d49dbda9b
Nonhlamhla <mkhonza.s@live.com>
2018-01-25 12:19:31 +0200

    Profile and Timetable

--- app/src/main/java/com/example/admin/apptoast/LandingBottomActivity.java
+++ app/src/main/java/com/example/admin/apptoast/LandingBottomActivity.java
@@ -1,14 +1,19 @@
 package com.example.admin.apptoast;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
-public class LandingBottomActivity extends AppCompatActivity {
+public class LandingBottomActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
 
 
@@ -22,15 +27,16 @@ public class LandingBottomActivity extends AppCompatActivity {
                 switch (item.getItemId()) {
 
                     case R.id.navigation_home:
-
                         fragment = new Landing();
                         loadFragment(fragment);
                         return true;
                     case R.id.navigation_dashboard:
-
+                        fragment = new TimeTableFragment();
+                        loadFragment(fragment);
                         return true;
                     case R.id.navigation_notifications:
-
+                        fragment = new Landing();
+                        loadFragment(fragment);
                         return true;
                 }
                 return false;
@@ -43,11 +49,21 @@ public class LandingBottomActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_bottom);
 
+
+        /**
+         * Toolbar
+         */
+        Toolbar toolbar = findViewById(R.id.bus_Schedule) ;
+        toolbar.setTitle("Bus Schedule");
+        toolbar.setTitleTextColor(Color.BLACK);
+
       loadFragment(new Landing());
 
 
+
+
        // mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
     private void loadFragment(Fragment fragment) {
@@ -58,4 +74,23 @@ public class LandingBottomActivity extends AppCompatActivity {
         transaction.commit();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+        searchView.setOnQueryTextListener(this);
+        return true;
+
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return true;
+    }
 }
--- app/src/main/java/com/example/admin/apptoast/Profile.java
+++ app/src/main/java/com/example/admin/apptoast/Profile.java
@@ -1,13 +1,30 @@
 package com.example.admin.apptoast;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Profile extends AppCompatActivity {
+
+ImageView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        back = findViewById(R.id.back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), LandingBottomActivity.class); //CAST ACTIVITY TO MANIFEST TO RESOLVE ERROR LERATO SAMBULA
+                startActivity(intent);
+            }
+        });
+
     }
 }
