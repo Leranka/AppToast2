a2584dec26a3404499abd714585c77dcb06fbf41
Lerato Sambula <sambula.lerato4@gmail.com>
2018-01-25 12:14:41 +0200

    Destination UI and Alert dialog

--- app/src/main/java/com/example/admin/apptoast/Destination.java
+++ app/src/main/java/com/example/admin/apptoast/Destination.java
@@ -3,21 +3,24 @@ package com.example.admin.apptoast;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class Destination extends AppCompatActivity{
+public class Destination extends AppCompatActivity {
 
     private Spinner spinner1;
     Context context;
     private Button btn_viewprice;
     AlertDialog.Builder builder1;
+    Toolbar myToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +34,19 @@ public class Destination extends AppCompatActivity{
             @Override
             public void onClick(View view) {
                 ViewDialog alert = new ViewDialog();
-                alert.showDialog(Destination.this, "Error de conexión al servidor");
+                alert.showDialog(Destination.this, "Your current Destination \n Where you are going \n Trip : Monthly \n Price: R980.00");
             }
         });
 
-
+        myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setTitle("Destination");
+        myToolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(myToolbar);
 
 
     }
+
+
     public void addListenerOnSpinnerItemSelection() {
         spinner1 = (Spinner) findViewById(R.id.spinner1);
         spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
@@ -46,7 +54,7 @@ public class Destination extends AppCompatActivity{
 
     public class ViewDialog {
 
-        public void showDialog(Activity activity, String msg){
+        public void showDialog(Activity activity, String msg) {
             final Dialog dialog = new Dialog(activity);
             dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
             dialog.setCancelable(false);
