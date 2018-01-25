13cda45b07904a8bb3efa5ac4abc5104e58d0b6b
Lerato Sambula <sambula.lerato4@gmail.com>
2018-01-25 08:40:25 +0200

    Destination functionality

--- app/src/main/java/com/example/admin/apptoast/Destination.java
+++ app/src/main/java/com/example/admin/apptoast/Destination.java
@@ -1,13 +1,72 @@
 package com.example.admin.apptoast;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class Destination extends AppCompatActivity {
+public class Destination extends AppCompatActivity{
+
+    private Spinner spinner1;
+    Context context;
+    private Button btn_viewprice;
+    AlertDialog.Builder builder1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_destination);
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        btn_viewprice = findViewById(R.id.btn_viewprice);
+
+        addListenerOnSpinnerItemSelection();
+        btn_viewprice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ViewDialog alert = new ViewDialog();
+                alert.showDialog(Destination.this, "Error de conexión al servidor");
+            }
+        });
+
+
+
+
     }
+    public void addListenerOnSpinnerItemSelection() {
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+    }
+
+    public class ViewDialog {
+
+        public void showDialog(Activity activity, String msg){
+            final Dialog dialog = new Dialog(activity);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setCancelable(false);
+            dialog.setContentView(R.layout.dialog);
+
+            TextView text = (TextView) dialog.findViewById(R.id.text_dialog);
+            text.setText(msg);
+
+            Button dialogButton = (Button) dialog.findViewById(R.id.btn_dialog);
+            dialogButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+
+            dialog.show();
+
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/admin/apptoast/Landing.java
+++ app/src/main/java/com/example/admin/apptoast/Landing.java
@@ -1,15 +1,18 @@
 package com.example.admin.apptoast;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class Landing extends Fragment{
 
-   // LinearLayout citytocity, putco, reavaya, metrobus;
-    //ImageView profile;
+    LinearLayout citytocity, putco, reavaya, metrobus;
+    ImageView profile;
     private View view;
 
     public Landing() {
@@ -21,51 +24,51 @@ public class Landing extends Fragment{
 
         view = inflater.inflate(R.layout.activity_landing, container, false);
 
-//        citytocity = view.findViewById(R.id.layout_citytocity);
-//        putco = view.findViewById(R.id.layout_putco);
-//        reavaya = view.findViewById(R.id.layout_reavaya);
-//        metrobus = view.findViewById(R.id.layout_metrobus);
-//        profile = view.findViewById(R.id.profile);
-//
-//        citytocity.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent i = new Intent(getActivity(),Destination.class);
-//                startActivity(i);
-//            }
-//        });
-//
-//        putco.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent i = new Intent(getActivity(),Destination.class);
-//                startActivity(i);
-//            }
-//        });
-//
-//        reavaya.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent i = new Intent(getActivity(),Destination.class);
-//                startActivity(i);
-//            }
-//        });
-//
-//        metrobus.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent i = new Intent(getActivity(),Destination.class);
-//                startActivity(i);
-//            }
-//        });
-//
-//        profile.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent i = new Intent(getActivity(),Profile.class);
-//                startActivity(i);
-//            }
-//        });
+        citytocity = view.findViewById(R.id.layout_citytocity);
+        putco = view.findViewById(R.id.layout_putco);
+        reavaya = view.findViewById(R.id.layout_reavaya);
+        metrobus = view.findViewById(R.id.layout_metrobus);
+        profile = view.findViewById(R.id.profile);
+
+        citytocity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(),Destination.class);
+                startActivity(i);
+            }
+        });
+
+        putco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(),Destination.class);
+                startActivity(i);
+            }
+        });
+
+        reavaya.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(),Destination.class);
+                startActivity(i);
+            }
+        });
+
+        metrobus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(),Destination.class);
+                startActivity(i);
+            }
+        });
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(),Profile.class);
+                startActivity(i);
+            }
+        });
         return view;
     }
 
